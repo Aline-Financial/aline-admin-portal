4801d1600bf13faf29d306bb182b152fc79e5880
smooth-dasilva <luan.dasilva@smoothstack.com>
2021-09-02 16:03:42 -0400

    AF-220-Jenkins-Employee: Fix passing env locally and not for CICD

--- sonar-project.js
+++ sonar-project.js
@@ -2,8 +2,8 @@ const sonarqubeScanner = require('sonarqube-scanner');
 require('dotenv').config()
 
 sonarqubeScanner({
-    serverUrl: `${process.env.SONAR_HOST_URL}`,
-    token:`${process.env.SONAR_TOKEN}`,
+    serverUrl: process.env.SONAR_HOST_URL,
+    token: process.env.SONAR_TOKEN,
     options: {
         'sonar.sources': 'src',
         'sonar.tests': 'src/tests',
